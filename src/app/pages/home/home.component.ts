3948c7da9f4ba2c3f4a43155ab9d633177f9031b
Kassia Albuquerque <kassiademaria@gmail.com>
2024-10-31 15:47:58 -0300

    feat(api): add integration to fetch Pokémon data from API
    - Integrated Pokémon API to dynamically fetch and display data for each Pokémon card.
    - Updated CardInfoComponent to retrieve Pokémon data using HttpClient and set properties.
    - Configured HomeComponent to loop through Pokémon data and render cards.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface PokemonData {
+  name: string;
+  description: string;
+  types: string[];
+  id: number;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  pokemonList: PokemonData[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchFirst151Pokemon();
+  }
+
+  fetchFirst151Pokemon() {
+    const pokemonRequests = [];
+    for (let i = 1; i <= 151; i++) {
+      pokemonRequests.push(this.http.get(`https://pokeapi.co/api/v2/pokemon/${i}`).toPromise());
+    }
+
+    Promise.all(pokemonRequests).then((responses: any[]) => {
+      this.pokemonList = responses.map((data: any) => ({
+        name: data.name,
+        description: `The Pokémon ${data.name} has ${data.types.length} type(s) and a base experience of ${data.base_experience}.`,
+        types: data.types.map((type: any) => type.type.name),
+        id: data.id,
+        imageUrl: data.sprites.front_default,
+      }));
+    });
+  }
+}
--- src/app/shared/components/card-info/card-info.component.ts
+++ src/app/shared/components/card-info/card-info.component.ts
@@ -1,19 +1,35 @@
-import { Component, Input } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card-info',
   templateUrl: './card-info.component.html',
   styleUrls: ['./card-info.component.scss'],
 })
-export class CardInfoComponent {
-  @Input() namePokemon: string = 'Undedfined';
-  @Input() description: string = 'Undedfined';
-  @Input() listOfPokemonTypes: string[] = ['Undedfined'];
+export class CardInfoComponent implements OnInit {
+  @Input() namePokemon: string = 'Undefined';
+  @Input() description: string = 'Undefined';
+  @Input() listOfPokemonTypes: string[] = ['Undefined'];
   @Input() posNumber: number = 0;
-  @Input() imageURL: string = '';
+  @Input() imageURL: string = ''; // Adicionando `imageURL` como um @Input()
+
+  pokemonData: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchPokemonData();
+  }
+
+  fetchPokemonData() {
+    this.http.get(`https://pokeapi.co/api/v2/pokemon/${this.namePokemon.toLowerCase()}`).subscribe((data) => {
+      this.pokemonData = data;
+      this.imageURL = this.pokemonData?.sprites?.other['official-artwork'].front_default;
+    });
+  }
 
   formattedPosNumber() {
-    // deixa no formato `#000`.
     return `#${this.posNumber.toString().padStart(3, '0')}`;
   }
 }
