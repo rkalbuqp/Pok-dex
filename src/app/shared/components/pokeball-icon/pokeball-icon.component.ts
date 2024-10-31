09ee2d5ea64b7751e9901370702d269529296f80
Kassia Albuquerque <kassiademaria@gmail.com>
2024-10-31 14:01:18 -0300

    feat(styles): adding component with responsive pokeball icon

---
+++ src/app/shared/components/pokeball-icon/pokeball-icon.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokeball-icon',
+  templateUrl: './pokeball-icon.component.html',
+  styleUrl: './pokeball-icon.component.scss',
+})
+export class PokeballIconComponent {}
